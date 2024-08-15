2f19793d74671e14a71325801f401ef225c275d4
Jae Jeong <jeongjj29@gmail.com>
2024-08-15 15:54:18 -0400

    add components and pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/meals")
+      .then((r) => r.json())
+      .then((data) => setMeals(data));
+  }, []);
+
   return <div>App</div>;
 }
 
