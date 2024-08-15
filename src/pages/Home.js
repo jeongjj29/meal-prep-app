380da55d1c508eda0e3c5cd6f8bc55b04f46369d
Jae Jeong <jeongjj29@gmail.com>
2024-08-15 18:36:12 -0400

    add meal cards on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MealsCollection from "../components/MealsCollection";
 
 function Home() {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    fetch("https://meal-app-server.onrender.com/meals")
+      .then((r) => r.json())
+      .then((data) => setMeals(data));
+  }, []);
+
   return (
     <div>
       <h1>Home</h1>
-      <MealsCollection />
+      <MealsCollection meals={meals} />
     </div>
   );
 }
