88c61ae8f30f5eaf243147ba5c71c92c153f717b
Jae Jeong <jeongjj29@gmail.com>
2024-08-16 17:05:53 -0400

    add blank div if no image

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -6,12 +6,16 @@ function Meals({ meal }) {
       <div className="w-56 h-20 flex items-center justify-center">
         <h3 className="meal-title text-wrap w-52">{meal.name}</h3>
       </div>
+      {meal.image ? (
+        <img
+          className="w-56 h-56 object-cover"
+          src={meal.image}
+          alt={meal.name}
+        />
+      ) : (
+        <div className="w-56 h-56"></div>
+      )}
 
-      <img
-        className="w-56 h-56 object-cover"
-        src={meal.image}
-        alt={meal.name}
-      />
       <h4 className="font-bold">Ingredients:</h4>
       <ul className="">
         {meal.ingredients.map((ingredient, index) => {
