d153c62c195912e55255776ce9fe597c9a09288c
Mark Beale <bealebeaverst@gmail.com>
2024-08-15 21:57:48 -0700

    filter started

--- src/components/MealsCollection.js
+++ src/components/MealsCollection.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Meals from "./Meals";
 
 function MealsCollection({ meals }) {
+  const [filter, setFilter] = useState("all");
+
+  const filteredMeals = filter === "all"
+  ? meals
+  : meals.filter(meal => meal.mealtime === filter);
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  }
+
   return (
-    <div className="flex flex-wrap ">
-      {meals.map((meal) => {
-        return <Meals key={meal.id} meal={meal} />;
-      })}
+    <div>
+      <div className="filter-container">
+        <label>Filter Meals:</label>
+        <select>
+          <option></option>
+          <option></option>
+          <option></option>
+        </select>
+      </div>
+      <div className="flex flex-wrap ">
+        {meals.map((meal) => {
+          return <Meals key={meal.id} meal={meal} />;
+        })}
+      </div>
     </div>
   );
 }
