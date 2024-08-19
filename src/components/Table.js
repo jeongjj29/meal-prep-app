6411c6e4fdefcb9c30d53e53e15ae271d92e0f5c
Mark Beale <bealebeaverst@gmail.com>
2024-08-18 18:55:06 -0700

    style additions

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,49 +2,49 @@ import React, { useState, useEffect } from "react";
 
 function Table({ mealPlan }) {
   return (
-    <table className="table-auto border-collapse border border-slate-500 w-9/12 mt-20 ml-auto mr-auto text-center">
-      <thead>
+    <table className="meal-plan-table">
+      <thead className="meal-plan-table-header">
         <tr>
-          <th></th>
-          <th className="border border-slate-500">Sunday</th>
-          <th className="border border-slate-500">Monday</th>
-          <th className="border border-slate-500">Tuesday</th>
-          <th className="border border-slate-500">Wednesday</th>
-          <th className="border border-slate-500">Thursday</th>
-          <th className="border border-slate-500">Friday</th>
-          <th className="border border-slate-500">Saturday</th>
+          <th className="meal-plan-table-cell"></th>
+          <th className="meal-plan-table-cell">Sunday</th>
+          <th className="meal-plan-table-cell">Monday</th>
+          <th className="meal-plan-table-cell">Tuesday</th>
+          <th className="meal-plan-table-cell">Wednesday</th>
+          <th className="meal-plan-table-cell">Thursday</th>
+          <th className="meal-plan-table-cell">Friday</th>
+          <th className="meal-plan-table-cell">Saturday</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th className="border border-slate-500">Breakfast</th>
-          <td className="border border-slate-500">{mealPlan[0].breakfast}</td>
-          <td className="border border-slate-500">{mealPlan[1].breakfast}</td>
-          <td className="border border-slate-500">{mealPlan[2].breakfast}</td>
-          <td className="border border-slate-500">{mealPlan[3].breakfast}</td>
-          <td className="border border-slate-500">{mealPlan[4].breakfast}</td>
-          <td className="border border-slate-500">{mealPlan[5].breakfast}</td>
-          <td className="border border-slate-500">{mealPlan[6].breakfast}</td>
+        <tr className="meal-plan-table-row-alt">
+          <th className="meal-plan-table-meal-header">Breakfast</th>
+          <td className="meal-plan-table-cell">{mealPlan[0].breakfast}</td>
+          <td className="meal-plan-table-cell">{mealPlan[1].breakfast}</td>
+          <td className="meal-plan-table-cell">{mealPlan[2].breakfast}</td>
+          <td className="meal-plan-table-cell">{mealPlan[3].breakfast}</td>
+          <td className="meal-plan-table-cell">{mealPlan[4].breakfast}</td>
+          <td className="meal-plan-table-cell">{mealPlan[5].breakfast}</td>
+          <td className="meal-plan-table-cell">{mealPlan[6].breakfast}</td>
         </tr>
         <tr>
-          <th>Lunch</th>
-          <td className="border border-slate-500">{mealPlan[0].lunch}</td>
-          <td className="border border-slate-500">{mealPlan[1].lunch}</td>
-          <td className="border border-slate-500">{mealPlan[2].lunch}</td>
-          <td className="border border-slate-500">{mealPlan[3].lunch}</td>
-          <td className="border border-slate-500">{mealPlan[4].lunch}</td>
-          <td className="border border-slate-500">{mealPlan[5].lunch}</td>
-          <td className="border border-slate-500">{mealPlan[6].lunch}</td>
+          <th className="meal-plan-table-meal-header">Lunch</th>
+          <td className="meal-plan-table-cell">{mealPlan[0].lunch}</td>
+          <td className="meal-plan-table-cell">{mealPlan[1].lunch}</td>
+          <td className="meal-plan-table-cell">{mealPlan[2].lunch}</td>
+          <td className="meal-plan-table-cell">{mealPlan[3].lunch}</td>
+          <td className="meal-plan-table-cell">{mealPlan[4].lunch}</td>
+          <td className="meal-plan-table-cell">{mealPlan[5].lunch}</td>
+          <td className="meal-plan-table-cell">{mealPlan[6].lunch}</td>
         </tr>
-        <tr>
-          <th className="border border-slate-500">Dinner</th>
-          <td className="border border-slate-500">{mealPlan[0].dinner}</td>
-          <td className="border border-slate-500">{mealPlan[1].dinner}</td>
-          <td className="border border-slate-500">{mealPlan[2].dinner}</td>
-          <td className="border border-slate-500">{mealPlan[3].dinner}</td>
-          <td className="border border-slate-500">{mealPlan[4].dinner}</td>
-          <td className="border border-slate-500">{mealPlan[5].dinner}</td>
-          <td className="border border-slate-500">{mealPlan[6].dinner}</td>
+        <tr className="meal-plan-table-row-alt">
+          <th className="meal-plan-table-meal-header">Dinner</th>
+          <td className="meal-plan-table-cell">{mealPlan[0].dinner}</td>
+          <td className="meal-plan-table-cell">{mealPlan[1].dinner}</td>
+          <td className="meal-plan-table-cell">{mealPlan[2].dinner}</td>
+          <td className="meal-plan-table-cell">{mealPlan[3].dinner}</td>
+          <td className="meal-plan-table-cell">{mealPlan[4].dinner}</td>
+          <td className="meal-plan-table-cell">{mealPlan[5].dinner}</td>
+          <td className="meal-plan-table-cell">{mealPlan[6].dinner}</td>
         </tr>
       </tbody>
     </table>
